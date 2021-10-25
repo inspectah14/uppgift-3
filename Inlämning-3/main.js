8ebc8cb78424a56d351754c16eb5f704ccf0bd56
inspectah14 <lambada_41@hotmail.com>
2021-10-25 16:05:26 +0200

    updated JS commit

--- Inlämning-3/main.js
+++ Inlämning-3/main.js
@@ -1,6 +1,7 @@
 let body = document.querySelector("body");
 let quizContainer = document.querySelector("#quiz");
 let endGameContainer = document.querySelector("#end-game");
+let resetContainer = document.querySelector("#reset-container");
 
 let darkButton = document.querySelector("#dark-button");
 darkButton.addEventListener("click", () => {
@@ -16,38 +17,55 @@ lightButton.addEventListener("click", () => {
     quizContainer.style.border = "2px solid black";
 })
 
-
-const answers = [
-    {
-        name: "quest-9",
-        correct: "qc9"
-    }
-];
-
 let userAnswers = [];
 
+let qOneAnswers = document.querySelectorAll("[name='quest-1']");
+let qTwoAnswers = document.querySelectorAll("[name='quest-2']");
+let qThreeAnswers = document.querySelectorAll("[name='quest-3']");
+let qFourAnswers = document.querySelectorAll("[name='quest-4']");
+let qFiveAnswers = document.querySelectorAll("[name='quest-5']");
+let qSixAnswers = document.querySelectorAll("[name='quest-6']");
+let qSevenAnswers = document.querySelectorAll("[name='quest-7']");
+let qEightAnswers = document.querySelectorAll("[name='quest-8']");
 let qNineAnswers = document.querySelectorAll("[name='quest-9']");
+let qTenAnswers = document.querySelectorAll("[name='quest-10']");
 
 const disable = () => {
     checkAnswersButton.disabled = true;
 }
 
+const checkAnswers = (btn) => {
+    if(btn.checked === true) {
+        userAnswers.push(btn.value);
+    }
+}
+
 let checkAnswersButton = document.querySelector("#checkAnswers-button");
 
 checkAnswersButton.addEventListener("click", () => {
-
-    qNineAnswers.forEach((btn) => {
-        if(btn.checked === true) {
-            userAnswers?.push(btn.value);
-            console.log(userAnswers);
-        }
-    })
+    qOneAnswers.forEach(checkAnswers);
+    qTwoAnswers.forEach(checkAnswers);
+    qThreeAnswers.forEach(checkAnswers);
+    qFourAnswers.forEach(checkAnswers);
+    qFiveAnswers.forEach(checkAnswers);
+    qSixAnswers.forEach(checkAnswers);
+    qSevenAnswers.forEach(checkAnswers);
+    qEightAnswers.forEach(checkAnswers);
+    qNineAnswers.forEach(checkAnswers);
+    qTenAnswers.forEach(checkAnswers);
+    let numberedAnswers = userAnswers.map(Number);
+    let reducer = (previousValue, currentValue) => previousValue + currentValue;
+    console.log(numberedAnswers.reduce(reducer));
+    let results = numberedAnswers.reduce(reducer);
+    let resultsHeading = document.createElement("h4");
+    resultsHeading.innerText = `Your final score: ${results}`;
+    endGameContainer.appendChild(resultsHeading);
     disable(); //förhindrar användaren att submitta fler svar och därmed addera till arrayen.
 })
 
 
 let resetButton = document.querySelector("#reset-button")
-endGameContainer.appendChild(resetButton);
+resetContainer.appendChild(resetButton);
 resetButton.addEventListener("click", () => {
     location.reload();
 });
